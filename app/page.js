da90e6a5d8ea1f657d5e402856aa5e5b8ba71399
Abdullah <abdullahshamoon24@gmail.com>
2024-12-19 11:04:42 +0530

    lib/mongodb.js created

--- app/page.js
+++ app/page.js
@@ -1,9 +1,20 @@
-import Image from "next/image";
+'use client';
+import { useState } from "react";
 
 export default function Home() {
+  const [url, setUrl] = useState("")
+  const [shortUrl, setShortUrl] = useState("")
+  const [generated, setGenerated] = useState(false)
+
+  
+
   return (
     <>
-     
+     <div>
+      <input type="text" placeholder="Enter your URL" value={url} onChange={e => setUrl(e.target.value)}/>
+      <input type="text" placeholder="Enter your preferred short URL" value={shortUrl} onChange={e => setShortUrl(e.target.value)} />
+      <button>Generate</button>
+     </div>
     </>
   );
 }
