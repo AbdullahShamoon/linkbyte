249399d456656e04cc26a1f700bfc7417c7e6db4
Abdullah <abdullahshamoon24@gmail.com>
2024-12-20 02:23:09 +0530

    all functionality added

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,3 +1,23 @@
+import clientPromise from "@/lib/mongodb"
+
 export async function POST(request) {
-    return Response.json({ message: "hello" })
+
+    const body = await request.json()
+    const client = await clientPromise
+    const db = client.db("linkbyte")
+    const collection = db.collection("links")
+
+    //check if short url exists
+    const existing = await collection.findOne({ shorturl: body.shorturl })
+    if (existing) {
+        return Response.json({ success: false , error: true , message: "shorturl already exists" })
+    }
+
+    const result = await collection.insertOne({
+        url: body.url,
+        shorturl: body.shorturl
+    })
+
+
+    return Response.json({ success: true , error: false , message: "URL generated successfully" })
 }
--- app/page.js
+++ app/page.js
@@ -1,20 +1,55 @@
 'use client';
 import { useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
   const [url, setUrl] = useState("")
-  const [shortUrl, setShortUrl] = useState("")
-  const [generated, setGenerated] = useState(false)
+  const [shorturl, setShorturl] = useState("")
+  const [generated, setGenerated] = useState()
 
-  
+  const generate = () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      "url": url,
+      "shorturl": shorturl
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
+    };
+
+    fetch("/api/generate", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        setGenerated(`${process.env.NEXT_PUBLIC_HOST}/${shorturl}`)
+        setShorturl("")
+        if(result.success==true)setUrl("")
+        console.log(result)
+        alert(result.message)
+      })
+      .catch((error) => console.error(error));
+  }
+
 
   return (
     <>
-     <div>
-      <input type="text" placeholder="Enter your URL" value={url} onChange={e => setUrl(e.target.value)}/>
-      <input type="text" placeholder="Enter your preferred short URL" value={shortUrl} onChange={e => setShortUrl(e.target.value)} />
-      <button>Generate</button>
-     </div>
+      <div className="flex flex-col items-center justify-center mt-5 bg-gray-50 min-w-fit w-1/2 mx-auto p-5 border rounded-3xl">
+        <input type="text" className="w-full py-1 px-2 rounded-2xl border mb-2 " placeholder="Enter your URL" value={url} onChange={e => setUrl(e.target.value)} />
+        <input type="text" className="w-full py-1 px-2 rounded-2xl border mb-2" placeholder="Enter your preferred short URL" value={shorturl} onChange={e => setShorturl(e.target.value)} />
+        <button onClick={generate} className="relative rounded-full px-3 py-2 overflow-hidden group bg-green-500 hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300">
+          <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+          <span className="relative">Generate</span>
+        </button>
+        {generated && <>
+        <span className="font-bold text-lg">Your Link</span>
+        <div><Link href={generated} className="font-prosto">{generated}</Link></div>
+        </>}
+      </div>
     </>
   );
 }
